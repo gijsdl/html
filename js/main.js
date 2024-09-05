fa8a4b9126f7c59841f244ce6f6db0001379f1dc
Gijs de Lange <g.de.lange@rocmondriaan.nl>
2024-09-05 12:24:12 +0200

    les 1 af

---
+++ js/main.js
@@ -0,0 +1,27 @@
+console.log("main loaded");
+
+// const x = 10;
+// const y = 17;
+
+// let answer = x + y;
+// alert(answer);
+// answer = y-x;
+// alert(answer);
+// answer = y % x;
+// alert(answer);
+
+// const x = "dit is een";
+// const y = "stuk tekst";
+
+// const string = x + y;
+
+// console.log(x + ' ' + y);
+
+// console.log("je score is " + (x + y));
+
+const x = 11;
+const y = 10;
+
+console.log(x >= y);
+console.log(x <= y);
+console.log(x != y);
